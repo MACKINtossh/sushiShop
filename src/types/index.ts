bed8004e6968bd455424b336438b679a0d7d3cbc
Jordan Fontaine <jordanmfontaine@gmail.com>
2023-06-29 18:16:08 -0500

    added types for the cart items, product card and product page

---
+++ src/types/index.ts
@@ -0,0 +1,15 @@
+export type StoredFile = {
+  id: string;
+  name: string;
+  url: string;
+};
+
+export type CartItem = {
+  productId: number;
+  quantity: number;
+  price: number;
+};
+
+export type ProductsPageProps = {};
+
+export type ProductsCardProps = {};
