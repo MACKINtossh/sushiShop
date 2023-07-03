1683bc878a22784436a8b310939f4b0d63584fcf
Jordan Fontaine <jordanmfontaine@gmail.com>
2023-07-03 17:12:24 -0500

    renamed pictures and added to public folder

---
+++ public/index.ts
@@ -0,0 +1,6 @@
+import crab from "./crab.png";
+import sushi from "./sushi-preview.png";
+import curry from "./rendang-preview.png";
+import pho from "./pho-preview.png";
+
+export { crab, sushi, curry, pho };
