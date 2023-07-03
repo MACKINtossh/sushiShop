5622ccb21a6c668f6c0e56030a53c099b26d11d2
Jordan Fontaine <jordanmfontaine@gmail.com>
2023-07-03 17:15:05 -0500

    hardcoded featured items

---
+++ config/carousel.ts
@@ -0,0 +1,22 @@
+import { CarouselProps } from "@/types";
+
+export const carouselprops: CarouselProps[] = [
+  {
+    id: "sushi",
+    title: "Sushi",
+    href: "/sushi",
+    imageUrl: "/public/sushi-preview.png",
+  },
+  {
+    id: "pho",
+    title: "Pho",
+    href: "/pho",
+    imageUrl: "/public/pho-preview.png",
+  },
+  {
+    id: "curry",
+    title: "Curry",
+    href: "/curry",
+    imageUrl: "/public/rendang-preview.png",
+  },
+];
